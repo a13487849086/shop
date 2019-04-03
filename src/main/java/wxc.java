30191035180b8c8f6297ddb3b05f489f2143a1f3
itwcxing <39087195+a13487849086@users.noreply.github.com>
2019-04-03 10:59:41 +0800

    Update wxc.java

--- src/main/java/wxc.java
+++ src/main/java/wxc.java
@@ -3,7 +3,8 @@ import java.sql.SQLOutput;
 public class wxc {
 
     public static void main(String[] args) {
-        String name;
+        String name="王丹";
         System.out.println("吴星辰666");
+        System.out.println(name);
     }
 }
