a675104302ee3e927bb2c909e96da1d46eb9c3f9
yang199865 <39737515+yang199865@users.noreply.github.com>
2019-04-03 14:56:54 +0800

    Update wxc.java

--- src/main/java/wxc.java
+++ src/main/java/wxc.java
@@ -3,7 +3,6 @@ import java.sql.SQLOutput;
 public class wxc {
 
     public static void main(String[] args) {
-        String name0="杨哥哥"；
         String name="王丹";
         System.out.println("吴星辰傻逼");
         System.out.println(name);
