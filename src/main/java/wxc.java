712770dcb274c0fd14006ecaf94af8a83e73002e
yjh <2205348277@qq.com>
2019-04-03 14:59:27 +0800

    update by y

--- src/main/java/wxc.java
+++ src/main/java/wxc.java
@@ -4,7 +4,7 @@ public class wxc {
 
     public static void main(String[] args) {
         String name="王丹";
-        System.out.println("吴星辰傻逼");
+        System.out.println("吴星辰傻逼东西。");
         System.out.println(name);
     }
 }
