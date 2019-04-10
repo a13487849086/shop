55f8b6d2c5ef2ec7ade291ce69afd1225a5a423c
a13487849086 <39087195+a13487849086@users.noreply.github.com>
2019-04-10 12:41:00 +0800

    环境1

---
+++ src/main/java/com/itwcxing/control/userControl.java
@@ -0,0 +1,19 @@
+package com.itwcxing.control;
+
+
+import com.itwcxing.common.Msg;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class userControl {
+
+    public Msg register(){
+
+
+        return null;
+    }
+
+
+}
